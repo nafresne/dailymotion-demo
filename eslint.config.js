e880fb5ec6d7aef9af2014fe26cb6b3bb5f44f1e
Nicolas Afresne <nicolas.afresne@hivebrite.com>
2025-11-02 20:07:59 +0100

    chore(eslint): ignore coverage folder

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,21 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
-import { defineConfig, globalIgnores } from "eslint/config";
-import eslintConfigPrettier from "eslint-config-prettier/flat";
-import pluginQuery from "@tanstack/eslint-plugin-query";
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import pluginQuery from '@tanstack/eslint-plugin-query';
 
 export default defineConfig([
-  ...pluginQuery.configs["flat/recommended"],
-  globalIgnores(["dist"]),
+  ...pluginQuery.configs['flat/recommended'],
+  globalIgnores(['dist', 'coverage']),
   {
-    files: ["**/*.{ts,tsx}"],
+    files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
-      reactHooks.configs["recommended-latest"],
+      reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
       eslintConfigPrettier,
     ],
