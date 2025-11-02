1673c4fc779a30ed1c62178a6ef12fa084a223cd
Nicolas Afresne <nicolas.afresne@hivebrite.com>
2025-11-02 18:39:13 +0100

    feat(videoCard): add img background and update title

--- src/components/videoCard.tsx
+++ src/components/videoCard.tsx
@@ -4,15 +4,15 @@ import { VideoModal } from '@/components/videoModal';
 
 export const VideoCard = ({ video }: { video: Video }) => (
   <VideoModal video={video}>
-    <Card className="relative aspect-video cursor-pointer">
+    <Card className="relative aspect-video cursor-pointer bg-gray-100 dark:bg-gray-900 overflow-hidden py-0">
       <img
         src={video.thumbnail_url}
         alt={video.title}
         className="absolute inset-0 h-full w-full object-contain"
       />
 
-      <CardHeader className="h-full flex items-start">
-        <CardTitle className="text-white drop-shadow-lg">
+      <CardHeader className="h-full flex items-start px-0 py-1">
+        <CardTitle className="text-white drop-shadow-lg px-1">
           <span className="absolute inset-0 -inset-x-2 -inset-y-1 bg-black/60 rounded -z-10" />
           {video.title}
         </CardTitle>
